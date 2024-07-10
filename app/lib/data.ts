412905d68bca07da54b36f8045b20d52f352045c
lsuhan00 <lsuhan00@hanmail.net>
2024-07-10 17:49:15 +0900

    next paging게시판

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    await new Promise((resolve) => setTimeout(resolve, 3000)); //suspence 동작을 위한 타임아웃
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -30,6 +26,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // suspence 동작을 위한 타임아웃
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
